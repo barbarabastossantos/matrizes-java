ad14b10b40a6f4a0a93fbc62f3169b24d12874ed
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-09-24 14:31:12 -0300

    Primeiro commit - iniciando exercicios de matrizes

---
+++ MatrizesJava/src/main/java/barbara/matrizesjava/MatrizesJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package barbara.matrizesjava;
+
+/**
+ *
+ * @author ADM
+ */
+public class MatrizesJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
