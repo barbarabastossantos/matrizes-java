c3c6e1e2ead8626cecdfae50e62d6dff289b561f
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-11-09 16:01:38 -0300

    exercitanto logica

---
+++ MatrizesJava/src/main/java/logica/Data.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import java.time.LocalDate;
+/**
+ *
+ * @author ADM
+ */
+public class Data {
+    public static void main(String[] args) {
+        
+        LocalDate d01=LocalDate.now();
+        System.out.println(d01);
+        
+    }
+    
+}
