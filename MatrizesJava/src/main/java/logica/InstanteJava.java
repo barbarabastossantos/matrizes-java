6baa434272e920b720196d5823a3bbe9cc48222a
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-11-09 16:17:22 -0300

    exercitanto logica

---
+++ MatrizesJava/src/main/java/logica/InstanteJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import java.time.Instant;
+/**
+ *
+ * @author ADM
+ */
+public class InstanteJava {
+    public static void main(String[] args) {
+        Instant d03=Instant.now();
+        System.out.println(d03);
+    }
+ 
+}
