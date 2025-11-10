7c45a7326af37492ca37ce433a2e59d37df5edb5
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-11-10 06:07:44 -0300

    exercitanto logica

---
+++ MatrizesJava/src/main/java/logica/Data7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import java.time.Instant;
+/**
+ *
+ * @author ADM
+ */
+public class Data7 {
+    public static void main(String[] args) {
+        Instant d=Instant.parse("2022-11-10T06:07:12Z");
+        System.out.println(d);
+    }
+ 
+}
