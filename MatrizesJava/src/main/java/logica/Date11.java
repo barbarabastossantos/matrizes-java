c0818242634ce9870b0c99ff92dc0890b14116c4
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-11-10 07:48:48 -0300

    exercitanto logica

---
+++ MatrizesJava/src/main/java/logica/Date11.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+/**
+ *
+ * @author ADM
+ */
+public class Date11 {
+    public static void main(String[] args) {
+        LocalDate d= LocalDate.of(2025, 11, 10);
+        LocalDateTime d2=LocalDateTime.of(2025, 11, 10, 7, 30);
+                
+        System.out.println(d);
+        System.out.println(d2);
+    }
+ 
+}
